359ce4a4edfcbd46f1ba040b29fe1b952d221891
xuhshen <xuhshen@163.com>
2019-04-11 15:31:17 +0800

    update contain restart

--- deployment.sh
+++ deployment.sh
@@ -23,24 +23,24 @@ docker run -d --name stock_mock -e IP=stock_mongo --restart unless-stopped --net
 #### update stock mock strategy
 
 docker rm -f stock_strategy
-docker run  -d --name stock_strategy -e IP=stock_mongo -e ACCOUNT=stock_mock_acc1  -e RATE=1 -e PRODUCTS=1 --network stockmock-net 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main.py
+docker run  -d --name stock_strategy -e IP=stock_mongo -e ACCOUNT=stock_mock_acc1  -e RATE=1 -e PRODUCTS=1 --network stockmock-net --restart unless-stopped 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main.py
 
 #### update 500etf mock strategy
 docker rm -f 500ETF_strategy
-docker run  -d --name 500ETF_strategy -e IP=stock_mongo -e ACCOUNT=stock_mock_acc2  -e NUMBER=2 --network stockmock-net 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main_etf.py
+docker run  -d --name 500ETF_strategy -e IP=stock_mongo -e ACCOUNT=stock_mock_acc2  -e NUMBER=2 --network stockmock-net  --restart unless-stopped 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main_etf.py
 
 
 #### update real account strategy(hangye zhishu)
 #hang ye zhishu 
 docker rm -f stock_strategy_35204099
-docker run -d --name stock_strategy_35204099 -e IP=stock_mongo -e ACCOUNT=account2 -e RATE=1 -e PRODUCTS=2 -e MOCK=False --network stockmock-net 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main.py
+docker run -d --name stock_strategy_35204099 -e IP=stock_mongo -e ACCOUNT=account2 -e RATE=1 -e PRODUCTS=2 -e MOCK=False --network stockmock-net --restart unless-stopped 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main.py
 
 #500 etf
 docker rm -f 500ETF_strategy_35200453 
-docker run  -d --name 500ETF_strategy_35200453 -e IP=stock_mongo -e ACCOUNT=account1  -e NUMBER=0.001 -e MOCK=False --network stockmock-net 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main_etf.py
+docker run  -d --name 500ETF_strategy_35200453 -e IP=stock_mongo -e ACCOUNT=account1  -e NUMBER=0.001 -e MOCK=False --network stockmock-net --restart unless-stopped 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main_etf.py
 
 docker rm -f 500ETF_strategy_35204819 
-docker run  -d --name 500ETF_strategy_35204819 -e IP=stock_mongo -e ACCOUNT=account3  -e NUMBER=0.001 -e MOCK=False --network stockmock-net 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main_etf.py
+docker run  -d --name 500ETF_strategy_35204819 -e IP=stock_mongo -e ACCOUNT=account3  -e NUMBER=0.001 -e MOCK=False --network stockmock-net --restart unless-stopped 127.0.0.1:5000/xuhshen/stock_mock:latest python /home/main_etf.py
 
 
 
